9a51817ffc31349f22452e76e1523fefc517f8db
sgolebiewski-intel <sebastianx.golebiewski@intel.com>
2023-10-05 11:04:55 +0200

    Add new Sphinx directive for tutorials in docs

--- docs/openvino_sphinx_theme/openvino_sphinx_theme/directives/code.py
+++ docs/openvino_sphinx_theme/openvino_sphinx_theme/directives/code.py
@@ -132,3 +132,84 @@ class Scrollbox(Directive):
         if self.content:
             self.state.nested_parse(self.content, self.content_offset, node)
         return [node]
+
+def visit_showcase(self, node):
+    attrs = {}
+    notebooks_repo = "https://github.com/openvinotoolkit/openvino_notebooks/blob/main/notebooks/"
+    notebooks_binder = "https://mybinder.org/v2/gh/openvinotoolkit/openvino_notebooks/HEAD?filepath=notebooks%2F"
+    notebooks_colab = "https://colab.research.google.com/github/openvinotoolkit/openvino_notebooks/blob/main/notebooks/"
+    git_badge = "<img class='showcase-badge' src='https://badgen.net/badge/icon/github?icon=github&amp;label' alt='Github'>"
+    binder_badge = "<img class='showcase-badge' src='https://mybinder.org/badge_logo.svg' alt='Binder'>"
+    colab_badge = "<img class='showcase-badge' src='https://camo.githubusercontent.com/84f0493939e0c4de4e6dbe113251b4bfb5353e57134ffd9fcab6b8714514d4d1/68747470733a2f2f636f6c61622e72657365617263682e676f6f676c652e636f6d2f6173736574732f636f6c61622d62616467652e737667' alt='Colab'>"
+
+    if "height" or "width" in node:
+        attrs["style"] = (
+            (("height:" + "".join(c for c in str(node["height"]) if c.isdigit()) + "px!important; " ) if "height" in node is not None else "")
+            + (("width:" + "".join(c for c in str(node["width"]) if c.isdigit()) ) if "width" in node is not None else "")
+            + (("px; " if node["width"].find("px") != -1 else "%;") if "width" in node is not None else "")
+        )
+    self.body.append("<div class='showcase-wrap'>")
+    self.body.append(self.starttag(node, "div", **attrs))
+    self.body.append(("<img " + (" class='" + (node["img-class"] + " showcase-img' ") if 'img-class' in node is not None else " class='showcase-img'") + "src='" + node["img"] + "' alt='"+os.path.basename(node["img"])+"' />") if "img" in node is not None else "")
+    self.body.append(("<div class='showcase-content'><div class='showcase-content-container'><h2 class='showcase-title'>" + node["title"] + "</h2>") if 'title' in node is not None else "")
+    self.body.append(("<a href='" + notebooks_repo + node["title"] + "/" + node["title"] + ".ipynb' target='_blank'>" + git_badge + "</a>") if 'title' in node is not None else "")
+    self.body.append(("<a href='" + notebooks_binder + node["title"] + "%2F" + node["title"] + ".ipynb' target='_blank'>" + binder_badge + "</a>") if 'title' in node is not None else "")
+    self.body.append(("<a href='" + notebooks_colab + node["title"] + "/" + node["title"] + ".ipynb' target='_blank'>" + colab_badge + "</a>") if 'title' in node is not None else "")
+
+
+def depart_showcase(self, node):
+    notebook_file = ("notebooks/" + node["title"] + "-with-output.html") if 'title' in node is not None else ""
+    link_title = (node["title"]) if 'title' in node is not None else "OpenVINO Interactive Tutorial"
+    self.body.append("</div><button class='showcase-button' type='button' title='" + link_title +
+                     "' onclick=\"location.href='" + notebook_file + "'\">Read more</a></div></div></div>\n")
+
+
+class Nodeshowcase(nodes.container):
+    def create_showcase_component(
+        rawtext: str = "",
+        **attributes,
+    ) -> nodes.container:
+        node = nodes.container(rawtext, is_div=True, **attributes)
+        return node
+
+
+class Showcase(Directive):
+    has_content = True
+    required_arguments = 0
+    optional_arguments = 1
+    final_argument_whitespace = True
+    option_spec = {
+        'class': directives.class_option,
+        'name': directives.unchanged,
+        'width': directives.length_or_percentage_or_unitless,
+        'height': directives.length_or_percentage_or_unitless,
+        'style': directives.unchanged,
+        'img': directives.unchanged,
+        'img-class': directives.unchanged,
+        'title': directives.unchanged,
+        'git': directives.unchanged,
+    }
+
+    has_content = True
+
+    def run(self):
+
+        classes = ['showcase']
+        node = Nodeshowcase("div", rawtext="\n".join(self.content), classes=classes)
+        if 'height' in self.options:
+            node['height'] = self.options['height']
+        if 'width' in self.options:
+            node['width'] = self.options['width']
+        if 'img' in self.options:
+            node['img'] = self.options['img']
+        if 'img-class' in self.options:
+            node['img-class'] = self.options['img-class']
+        if 'title' in self.options:
+            node['title'] = self.options['title']
+        if 'git' in self.options:
+            node['git'] = self.options['git']
+        node['classes'] += self.options.get('class', [])
+        self.add_name(node)
+        if self.content:
+            self.state.nested_parse(self.content, self.content_offset, node)
+        return [node]
