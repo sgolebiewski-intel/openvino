4f2660c94dfa57b829c0dc916f8b05597e18f618
sgolebiewski-intel <sebastianx.golebiewski@intel.com>
2024-05-20 08:51:17 +0200

    Update shortcuts

--- docs/openvino_sphinx_theme/openvino_sphinx_theme/__init__.py
+++ docs/openvino_sphinx_theme/openvino_sphinx_theme/__init__.py
@@ -8,8 +8,17 @@ from docutils.parsers import rst
 from pathlib import Path
 from sphinx.util import logging
 from .directives.code import DoxygenSnippet, Scrollbox, Nodescrollbox, visit_scrollbox, depart_scrollbox
+import pydata_sphinx_theme
 
 SPHINX_LOGGER = logging.getLogger(__name__)
+
+
+pydata_sphinx_theme.shortcuts = [
+    ("twitter_url", "fa-brands fa-square-twitter", "Twitter"),
+    ("bitbucket_url", "fa-brands fa-bitbucket", "Bitbucket"),
+    ("gitlab_url", "fa-brands fa-square-gitlab", "GitLab"),
+    ("github_url", "squalus-github", "GitHub"),
+]
 
 
 def setup_edit_url(app, pagename, templatename, context, doctree):
