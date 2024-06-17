0b4057095e524d131760d0e805f16541b068f7dd
sgolebiewski-intel <sebastianx.golebiewski@intel.com>
2024-06-17 07:59:24 +0200

    Fix

--- docs/openvino_sphinx_theme/openvino_sphinx_theme/directives/code.py
+++ docs/openvino_sphinx_theme/openvino_sphinx_theme/directives/code.py
@@ -44,15 +44,14 @@ class DoxygenSnippet(LiteralInclude):
                 rel_filename, filename = self.env.relfn2path(self.arguments[0])
             self.env.note_dependency(rel_filename)
 
-
             if not os.path.exists(filename):
-                raise self.error('The "%s" snippet file does not exist '
-                                 'or the specified path is invalid.' % filename)
+                   logger.error(__('The %s snippet file does not exit, '
+                                     'or the specified path is invalid.'), filename)
             else:
                 reader = LiteralIncludeReader(filename, self.options, self.config)
                 text, lines = reader.read(location=location)
-                retnode = nodes.literal_block(text, text, source=filename)  # type: Element
                 if retnode:
+                    retnode = nodes.literal_block(text, text, source=filename)  # type: Element
                     retnode['force'] = 'force' in self.options
                     self.set_source_info(retnode)
                     if self.options.get('diff'):  # if diff is set, set udiff
